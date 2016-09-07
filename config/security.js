f56056b9185264683728d78ea6a2d3d628e82e65
Nathan Friedly <nathan@nfriedly.com>
2016-09-07 10:57:55 -0400

    Adding loose CSP header

--- config/security.js
+++ config/security.js
@@ -28,10 +28,55 @@ module.exports = function (app) {
   app.enable('trust proxy');
 
   // 1. redirects http to https
-  app.use(secure());
+  //app.use(secure());
 
-  // 2. helmet with defaults
-  app.use(helmet());
+  // 2. helmet with custom CSP policy
+  var cspReportUrl = '/report-csp-violation';
+  app.use(helmet({
+    contentSecurityPolicy: {
+      // Specify directives as normal.
+      directives: {
+        defaultSrc: ["'self'"], // default value for unspecified directives that end in "src"
+        // underscore requires unsafe-eval, google analytics needs unsafe-inline
+        scriptSrc: ["'self'", "'unsafe-eval'", "'unsafe-inline'", 'code.jquery.com', 'cdnjs.cloudflare.com/', 'www.google-analytics.com'],
+        //styleSrc: ["'self'"], // no inline css
+        imgSrc: ["'self'", 'www.google-analytics.com'], // note that * doesn't include 'data:'
+        connectSrc: ["'self'", '*.watsonplatform.net'], // ajax domains
+        //fontSrc: ["'self'"], // cdn?
+        objectSrc: [], // embeds (e.g. flash)
+        //mediaSrc: ["'self'", '*.watsonplatform.net'], // allow watson TTS streams
+        childSrc: [], // child iframes
+        frameAncestors: [], // parent iframes
+        formAction: ["'self'"], // where can forms submit to
+        pluginTypes: [], // e.g. flash, pdf
+        //sandbox: ['allow-forms', 'allow-scripts', 'allow-same-origin'], // options: allow-forms allow-same-origin allow-scripts allow-top-navigation
+        reportUri: cspReportUrl
+      },
+
+      // Set to true if you only want browsers to report errors, not block them.
+      // You may also set this to a function(req, res) in order to decide dynamically
+      // whether to use reportOnly mode, e.g., to allow for a dynamic kill switch.
+      reportOnly: false,
+
+      // Set to true if you want to blindly set all headers: Content-Security-Policy,
+      // X-WebKit-CSP, and X-Content-Security-Policy.
+      setAllHeaders: false,
+
+      // Set to true if you want to disable CSP on Android where it can be buggy.
+      disableAndroid: false,
+
+      // Set to false if you want to completely disable any user-agent sniffing.
+      // This may make the headers less compatible but it will be much faster.
+      // This defaults to `true`.
+      browserSniff: true
+    }
+  }));
+  // endpoint for browsers to report CSP violations to
+  app.post(cspReportUrl, function(req, res) {
+    console.log('Content Security Policy Violation:\n', req.body);
+    res.status(204).send(); // 204 = No Content
+  });
+
 
   // 3. setup cookies
   var secret = Math.random().toString(36).substring(7);
